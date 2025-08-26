1e5cb2a2ff3477f8b1ca7748e7a6a99b3eb7ec2c
Otgonjargal S <otgoodiesel@gmail.com>
2025-08-26 16:27:52 +0800

    user and message for socket

--- socket-server/src/models/index.js
+++ socket-server/src/models/index.js
@@ -1,8 +1,16 @@
 import User from "./User.js";
 import Message from "./Message.js";
 
-// Associations
-Message.belongsTo(User, { foreignKey: "senderId", as: "sender" });
-Message.belongsTo(User, { foreignKey: "receiverId", as: "receiver" });
+// One user can send many messages
+User.hasMany(Message, { foreignKey: "sender_id", as: "sentMessages" });
+
+// One user can receive many messages
+User.hasMany(Message, { foreignKey: "recipient_id", as: "receivedMessages" });
+
+// Each message belongs to a sender
+Message.belongsTo(User, { foreignKey: "sender_id", as: "sender" });
+
+// Each message belongs to a recipient
+Message.belongsTo(User, { foreignKey: "recipient_id", as: "recipient" });
 
 export { User, Message };
