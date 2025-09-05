64b50c98bf84f20c6541099ef3430809139b7de4
Otgonjargal S <otgoodiesel@gmail.com>
2025-09-05 14:21:09 +0800

    fixing bug i made

--- frontend/src/app/(authorized)/(common)/research/layout.js
+++ frontend/src/app/(authorized)/(common)/research/layout.js
@@ -8,9 +8,9 @@ export default async function InvestigationInfoLayout({ children }) {
 
   //not for roles.PLATFORM_ADMIN
   if (
-    user.role !== roles.INVESTIGATOR ||
-    user.role !== roles.INV_ADMIN ||
-    user.role !== roles.COPYRIGHT_HOLDER ||
+    user.role !== roles.INVESTIGATOR &&
+    user.role !== roles.INV_ADMIN &&
+    user.role !== roles.COPYRIGHT_HOLDER &&
     user.role !== roles.RESEARCHER
   ) {
     notFound();
