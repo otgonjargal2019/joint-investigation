7f17da3159668ce7929f65d795ce193a1b627e3c
Tamir Chuluunbaatar <tamirchuba@gmail.com>
2025-09-10 13:09:24 +0800

    formatting

--- backend/src/main/java/com/lsware/joint_investigation/cases/service/CaseService.java
+++ backend/src/main/java/com/lsware/joint_investigation/cases/service/CaseService.java
@@ -30,109 +30,111 @@ import com.lsware.joint_investigation.user.repository.UserRepository;
 @Service
 public class CaseService {
 
-    @Autowired
-    private CaseRepository caseRepository;
+	@Autowired
+	private CaseRepository caseRepository;
 
-    @Autowired
-    private UserRepository userRepository;
-
-    @PreAuthorize("hasRole('INV_ADMIN')")
-    @Transactional
-    public MappingJacksonValue createCase(CreateCaseRequest request) {
-        CustomUser currentUser = (CustomUser) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        UUID currentUserId = currentUser.getId();
-        Users creator = userRepository.findByUserId(currentUserId)
-                .orElseThrow(() -> new RuntimeException("User not found"));
-
-        Case caseToSave = null;
-        if (request.getCaseId() != null) {
-            caseToSave = caseRepository.findById(UUID.fromString(request.getCaseId()))
-                    .orElseThrow(() -> new RuntimeException("Parent case not found"));
-        } else {
-			caseToSave = new Case();
-        }
-        caseToSave.setCaseName(request.getCaseName());
-        caseToSave.setCaseOutline(request.getCaseOutline());
-        caseToSave.setContentType(request.getContentType());
-        caseToSave.setInfringementType(request.getInfringementType());
-        caseToSave.setRelatedCountries(request.getRelatedCountries());
-        caseToSave.setPriority(request.getPriority());
-        caseToSave.setStatus(CASE_STATUS.OPEN);
-        caseToSave.setInvestigationDate(request.getInvestigationDate());
-        caseToSave.setCreator(creator);
-        caseToSave.setCreatedAt(LocalDateTime.now());
-        caseToSave.setUpdatedAt(LocalDateTime.now());
-        caseToSave.setEtc(request.getEtc());
-
-        Case savedCase = caseRepository.save(caseToSave);
-        CaseDto caseDto = savedCase.toDto();
-
-        MappingJacksonValue mapping = new MappingJacksonValue(caseDto);
-
-        SimpleBeanPropertyFilter userFilter = SimpleBeanPropertyFilter
-                .filterOutAllExcept("userId", "role", "loginId", "nameKr", "nameEn", "email", "phone", "country", "department", "status");
-
-        FilterProvider filters = new SimpleFilterProvider()
-                .addFilter("UserFilter", userFilter);
-
-        mapping.setFilters(filters);
-
-        return mapping;
-    }
+	@Autowired
+	private UserRepository userRepository;
 
 	@PreAuthorize("hasRole('INV_ADMIN')")
-    public MappingJacksonValue getCaseList(UUID userId, String name, CASE_STATUS status, Pageable pageable) {
-        Map<String, Object> result = caseRepository.getCaseList(userId, name, status, pageable);
+	@Transactional
+	public MappingJacksonValue createCase(CreateCaseRequest request) {
+		CustomUser currentUser = (CustomUser) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		UUID currentUserId = currentUser.getId();
+		Users creator = userRepository.findByUserId(currentUserId)
+				.orElseThrow(() -> new RuntimeException("User not found"));
 
-        @SuppressWarnings("unchecked")
-        List<Case> records = (List<Case>)result.get("rows");
-        List<CaseDto> dtos = records.stream().map(Case::toDto).toList();
+		Case caseToSave = null;
+		if (request.getCaseId() != null) {
+			caseToSave = caseRepository.findById(UUID.fromString(request.getCaseId()))
+					.orElseThrow(() -> new RuntimeException("Parent case not found"));
+		} else {
+			caseToSave = new Case();
+		}
+		caseToSave.setCaseName(request.getCaseName());
+		caseToSave.setCaseOutline(request.getCaseOutline());
+		caseToSave.setContentType(request.getContentType());
+		caseToSave.setInfringementType(request.getInfringementType());
+		caseToSave.setRelatedCountries(request.getRelatedCountries());
+		caseToSave.setPriority(request.getPriority());
+		caseToSave.setStatus(CASE_STATUS.OPEN);
+		caseToSave.setInvestigationDate(request.getInvestigationDate());
+		caseToSave.setCreator(creator);
+		caseToSave.setCreatedAt(LocalDateTime.now());
+		caseToSave.setUpdatedAt(LocalDateTime.now());
+		caseToSave.setEtc(request.getEtc());
+
+		Case savedCase = caseRepository.save(caseToSave);
+		CaseDto caseDto = savedCase.toDto();
+
+		MappingJacksonValue mapping = new MappingJacksonValue(caseDto);
+
+		SimpleBeanPropertyFilter userFilter = SimpleBeanPropertyFilter
+				.filterOutAllExcept("userId", "role", "loginId", "nameKr", "nameEn", "email", "phone", "country",
+						"department", "status");
+
+		FilterProvider filters = new SimpleFilterProvider()
+				.addFilter("UserFilter", userFilter);
+
+		mapping.setFilters(filters);
+
+		return mapping;
+	}
+
+	@PreAuthorize("hasRole('INV_ADMIN')")
+	public MappingJacksonValue getCaseList(UUID userId, String name, CASE_STATUS status, Pageable pageable) {
+		Map<String, Object> result = caseRepository.getCaseList(userId, name, status, pageable);
+
+		@SuppressWarnings("unchecked")
+		List<Case> records = (List<Case>) result.get("rows");
+		List<CaseDto> dtos = records.stream().map(Case::toDto).toList();
 
 		MappingJacksonValue mapping = new MappingJacksonValue(Map.of(
-			"rows", dtos,
-			"total", result.get("total")
-        ));
+				"rows", dtos,
+				"total", result.get("total")));
 
-        SimpleBeanPropertyFilter userFilter = SimpleBeanPropertyFilter
-                .filterOutAllExcept("userId", "role", "loginId", "nameKr", "nameEn", "email", "phone", "country", "department", "status");
+		SimpleBeanPropertyFilter userFilter = SimpleBeanPropertyFilter
+				.filterOutAllExcept("userId", "role", "loginId", "nameKr", "nameEn", "email", "phone", "country",
+						"department", "status");
 
-        FilterProvider filters = new SimpleFilterProvider()
-                .addFilter("UserFilter", userFilter);
+		FilterProvider filters = new SimpleFilterProvider()
+				.addFilter("UserFilter", userFilter);
 
-        mapping.setFilters(filters);
+		mapping.setFilters(filters);
 
-        return mapping;
-    }
+		return mapping;
+	}
 
 	public MappingJacksonValue getCaseById(UUID caseId) {
-        // Fetch the case with latest investigation record
-        Case caseEntity = caseRepository.findById(caseId)
-                                        .orElseThrow(() -> new RuntimeException("Case not found"));
+		// Fetch the case with latest investigation record
+		Case caseEntity = caseRepository.findById(caseId)
+				.orElseThrow(() -> new RuntimeException("Case not found"));
 
-        CaseDto dto = caseEntity.toDto();
+		CaseDto dto = caseEntity.toDto();
 
 		if (caseEntity.getInvestigationRecords() != null) {
 			List<InvestigationRecordDto> records = caseEntity.getInvestigationRecords()
-				.stream()
-				.map(r -> {
-					InvestigationRecordDto rdto = r.toDto();
-					rdto.setCaseId(caseEntity.getCaseId()); // avoid recursion
-					return rdto;
-				})
-				.collect(Collectors.toList());
+					.stream()
+					.map(r -> {
+						InvestigationRecordDto rdto = r.toDto();
+						rdto.setCaseId(caseEntity.getCaseId()); // avoid recursion
+						return rdto;
+					})
+					.collect(Collectors.toList());
 			dto.setInvestigationRecords(records);
 		}
 
 		MappingJacksonValue mapping = new MappingJacksonValue(dto);
 
-        SimpleBeanPropertyFilter userFilter = SimpleBeanPropertyFilter
-                .filterOutAllExcept("userId", "role", "loginId", "nameKr", "nameEn", "email", "phone", "country", "department", "status");
+		SimpleBeanPropertyFilter userFilter = SimpleBeanPropertyFilter
+				.filterOutAllExcept("userId", "role", "loginId", "nameKr", "nameEn", "email", "phone", "country",
+						"department", "status");
 
-        FilterProvider filters = new SimpleFilterProvider()
-                .addFilter("UserFilter", userFilter);
+		FilterProvider filters = new SimpleFilterProvider()
+				.addFilter("UserFilter", userFilter);
 
-        mapping.setFilters(filters);
+		mapping.setFilters(filters);
 
-        return mapping;
-    }
+		return mapping;
+	}
 }
